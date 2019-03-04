453044c782bcde90ebb6217264a7e591bd503293
Michael Yankura <myankura210@gmail.com>
2019-03-03 22:34:40 -0600

    Not quite complete

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,21 +1,37 @@
 const restaurant = {
     name: "Bob's Burgers",
-    placeOrder: function () {
+    orders: [],
+    placeOrder: function (meal) {
+        this.orders.push(meal);
+    },
+    getOrders: function () {
+        this.orders.values;
     }
 }
-
 const chickenComboMeal = {
-  
-
+    sandwhichType: "Chicken",
+    fries: true,
+    drinkSize: "Medium"
 }
-function orders(meal)  {
-
+const cheeseBurgerComboMeal = {
+    sandwhichType: "Beef",
+    fries: true,
+    drinkSize: "Large"
 }
-
+const veganComboMeal = {
+    sandwhichType: "Vegan",
+    fries: false,
+    drinkSize: "Medium"
+}
+const fishComboMeal = {
+    sandwhichType: "Fish",
+    fries: true,
+    drinkSize: "Large"
+}
 // Place an order
 restaurant.placeOrder();
 
 // Invoke the function to return the list of all orders
-
+restaurant.getOrders();
 // Output all orders to the console using console.table()
 console.table(orders);
